41b143f1552f9634c3c18bf481216301ca99388a
Erick Márquez <erick.marquez@britecore.com>
2018-06-15 15:55:13 -0700

    Optimizado el tamaño del build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Element from 'element-ui';
+import {
+  Header,
+  Container,
+  Main,
+  Table,
+  TableColumn,
+  Progress,
+} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
@@ -9,7 +16,12 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-Vue.use(Element);
+Vue.use(Header);
+Vue.use(Container);
+Vue.use(Main);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Progress);
 
 /* eslint-disable no-new */
 new Vue({
